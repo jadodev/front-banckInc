f5520cc0cf2cc497ffdbdd13452b9c3429d09c3a
Jonathan Urrego <jadougdev@gmail.com>
2025-03-12 04:38:13 -0500

    card form component

---
+++ front-bank/src/app/pages/card/card.component.ts
@@ -0,0 +1,74 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+interface CardResponse {
+  cardNumber: string;
+  titularName: string;
+  expirationDate: string;
+  cardType: string;
+  balance: number;
+}
+
+@Component({
+  selector: 'card-form',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './card-form.component.html',
+  styleUrl: './card-form.component.css'
+})
+  export class CardFormComponent {
+    cardForm: FormGroup;
+  
+    constructor(
+      private fb: FormBuilder,
+      private http: HttpClient,
+      private router: Router, 
+      private cookieService: CookieService
+    ) {
+      this.cardForm = this.fb.group({
+        productId: ['', [Validators.required, Validators.minLength(6)]], 
+        titularName: ['', Validators.required],
+        cardType: ['CREDITO', Validators.required]
+      });
+    }
+  
+    onSubmit() {
+
+      if (this.cardForm.invalid) {
+        alert('Por favor, completa todos los campos correctamente.');
+        return;
+      }
+  
+      const formData = this.cardForm.value;
+
+      const userId = this.cookieService.get('userId');
+
+      if (!userId) {
+        alert('No se encontró un usuario registrado. Por favor, regístrate primero.');
+        return;
+      }
+
+      const cardNumber = this.cookieService.get('cardNumber');
+      if (cardNumber) {
+        this.router.navigate(['/profile']);
+      } else {
+        this.http.post<CardResponse>('http://localhost:8080/api/cards', formData).subscribe({
+          next: (response) => {
+            if (response) {
+              this.cookieService.set('cardNumber', response.cardNumber); 
+              this.cookieService.set('userId', userId);
+              this.cardForm.reset();
+              this.router.navigate(['/profile']);
+            }
+          },
+          error: (error) => {
+            console.error('Error al crear la tarjeta:', error);
+            alert('Error al crear la tarjeta');
+          }
+        });
+      }
+    }
+  }
