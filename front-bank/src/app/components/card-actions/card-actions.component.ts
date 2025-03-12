c0d2a6521abded083a7986e72b90a5db97558492
Jonathan Urrego <jadougdev@gmail.com>
2025-03-12 05:27:39 -0500

    card-actions

---
+++ front-bank/src/app/components/card-actions/card-actions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-actions',
+  imports: [],
+  templateUrl: './card-actions.component.html',
+  styleUrl: './card-actions.component.css'
+})
+export class CardActionsComponent {
+
+}
