26e6eb3ba27c20eb85d852d5770fba4568b85a0e
Jonathan Urrego <jadougdev@gmail.com>
2025-03-12 04:52:07 -0500

    profile features

--- front-bank/src/app/app.routes.ts
+++ front-bank/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { CardFormComponent } from './pages/card/card.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth', pathMatch: 'full' },
     { path: 'auth', component: AuthComponent },
-    { path: 'create-card', component: CardFormComponent }
+    { path: 'create-card', component: CardFormComponent },
+    { path: 'profile', component: ProfileComponent },
 ];
---
+++ front-bank/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  cardNumber: string | null = null;
+  cardInfo: any = null; 
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.cardNumber = this.getCookie('cardNumber');
+    console.log(this.cardNumber)
+
+    if (this.cardNumber) {
+      this.http.get<any>(`http://localhost:8080/api/cards/${this.cardNumber}`).subscribe({
+        next: (response) => {
+          this.cardInfo = response;
+        },
+        error: (error) => {
+          console.error('Error al obtener los detalles de la tarjeta:', error);
+          alert('No se pudo obtener la información de la tarjeta');
+        }
+      });
+    } else {
+      alert('No se encontró un número de tarjeta.');
+      this.router.navigate(['/create-card']);
+    }
+  }
+
+  getCookie(name: string): string | null {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift() ?? null;
+    return null;
+  }
+}
