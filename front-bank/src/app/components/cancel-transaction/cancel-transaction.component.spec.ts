b6ea0350add60fe72bd3a2221ddd499d05f49539
Jonathan Urrego <jadougdev@gmail.com>
2025-03-12 14:20:41 -0500

    docker

--- front-bank/src/app/components/cancel-transaction/cancel-transaction.component.spec.ts
+++ front-bank/src/app/components/cancel-transaction/cancel-transaction.component.spec.ts
@@ -1,23 +1,60 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CancelTransactionComponent } from './cancel-transaction.component';
+import { CancelTransaction } from '../../services/transactions.service';
+import { of, throwError } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 describe('CancelTransactionComponent', () => {
   let component: CancelTransactionComponent;
   let fixture: ComponentFixture<CancelTransactionComponent>;
+  let transactionService: jasmine.SpyObj<CancelTransaction>;
 
   beforeEach(async () => {
+    const transactionServiceSpy = jasmine.createSpyObj('CancelTransaction', ['cancelTransaction']);
+
     await TestBed.configureTestingModule({
-      imports: [CancelTransactionComponent]
+      imports: [CommonModule, FormsModule, CancelTransactionComponent],
+      providers: [
+        { provide: CancelTransaction, useValue: transactionServiceSpy }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(CancelTransactionComponent);
     component = fixture.componentInstance;
+    transactionService = TestBed.inject(CancelTransaction) as jasmine.SpyObj<CancelTransaction>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show error message if transactionId is empty', () => {
+    component.transactionId = '';
+    component.cancelTransaction();
+    expect(component.message).toBe('Ingrese un ID de transacción válido.');
+  });
+
+  it('should call cancelTransaction and handle success', () => {
+    transactionService.cancelTransaction.and.returnValue(of({}));
+    component.transactionId = '123';
+    component.cancelTransaction();
+    expect(component.isLoading).toBeTrue();
+    expect(transactionService.cancelTransaction).toHaveBeenCalledWith('123');
+    expect(component.message).toBe('Transacción anulada con éxito.');
+    expect(component.isLoading).toBeFalse();
+    expect(component.transactionId).toBe('');
+  });
+
+  it('should call cancelTransaction and handle error', () => {
+    transactionService.cancelTransaction.and.returnValue(throwError(() => new Error('Error')));
+    component.transactionId = '123';
+    component.cancelTransaction();
+    expect(component.isLoading).toBeTrue();
+    expect(transactionService.cancelTransaction).toHaveBeenCalledWith('123');
+    expect(component.message).toBe('Error al anular la transacción. Intenta nuevamente.');
+    expect(component.isLoading).toBeFalse();
   });
 });
--- front-bank/src/app/components/card-actions/card-actions.component.spec.ts
+++ front-bank/src/app/components/card-actions/card-actions.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CardActionsComponent } from './card-actions.component';
+import { CommonModule } from '@angular/common';
+import { RechargerComponent } from '../recharger/recharger.component';
+import { TransactionListComponent } from '../transaction-list/transaction-list.component';
+import { CancelTransactionComponent } from '../cancel-transaction/cancel-transaction.component';
 
 describe('CardActionsComponent', () => {
   let component: CardActionsComponent;
@@ -8,7 +11,7 @@ describe('CardActionsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CardActionsComponent]
+      imports: [CommonModule, RechargerComponent, TransactionListComponent, CancelTransactionComponent, CardActionsComponent]
     })
     .compileComponents();
 
@@ -19,5 +22,38 @@ describe('CardActionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle showRechargeForm on onReload', () => {
+    component.showRechargeForm = false;
+    component.onReload();
+    expect(component.showRechargeForm).toBeTrue();
+    expect(component.showTransactions).toBeFalse();
+    expect(component.cancelTransactions).toBeFalse();
+
+    component.onReload();
+    expect(component.showRechargeForm).toBeFalse();
+  });
+
+  it('should toggle showTransactions on onViewTransactions', () => {
+    component.showTransactions = false;
+    component.onViewTransactions();
+    expect(component.showTransactions).toBeTrue();
+    expect(component.showRechargeForm).toBeFalse();
+    expect(component.cancelTransactions).toBeFalse();
+
+    component.onViewTransactions();
+    expect(component.showTransactions).toBeFalse();
+  });
+
+  it('should toggle cancelTransactions on onCancelTransaction', () => {
+    component.cancelTransactions = false;
+    component.onCancelTransaction();
+    expect(component.cancelTransactions).toBeTrue();
+    expect(component.showRechargeForm).toBeFalse();
+    expect(component.showTransactions).toBeFalse();
+
+    component.onCancelTransaction();
+    expect(component.cancelTransactions).toBeFalse();
   });
 });
